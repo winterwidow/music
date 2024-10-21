6650f5cbd5ccc0c75d646126b38e89fbd6a031ea
Naija <naijahodigere11@gmail.com>
2024-10-21 10:29:24 +0530

    21/10

--- songs/urls.py
+++ songs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('classify/', views.classify_song, name='classify_song'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -6,15 +6,17 @@ from sklearn.neighbors import KNeighborsClassifier
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='6743d0e5040a4100b74091667f3a8991',client_secret='2608e96d0a1a4d6e912fb9a6482510c7'))  #spotify api
+client_id=open('spotify_client_id.txt')
+client_secret=open('spotify_client_secret.txt')
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,client_secret=client_secret))  #spotify api
 
 def home(request):
     return render(request, 'home.html')
 
 #using the api classify the genre
 def classify_genre(song_title,artist):
-    results = sp.search(q=f'track:{song_title} artist:{artist}', type='track', limit=1) #using the api to search for songs 
+
+    results = sp.search(q=f'track:{song_title} artist:{artist}', type='track', limit=1) #dictionary  
 
     if results['tracks']['items']:
 
@@ -23,12 +25,14 @@ def classify_genre(song_title,artist):
         artist_info = sp.artist(artist_id)
         genre = artist_info['genres'][0] if artist_info['genres'] else 'Unknown'
         return genre
+    
     else:
         return 'Unknown'
     
 #use the spotify api to generate similar songs
 def recommend_similar_songs(genre):
-    results = sp.recommendations(seed_genres=[genre], limit=5) #using spotify api
+
+    results = sp.recommendations(seed_genres=[genre], limit=5) #results is a dictionary having tracks and seeds as keys
     similar_songs = []
 
     for track in results['tracks']:
@@ -38,19 +42,35 @@ def recommend_similar_songs(genre):
             'artist': ', '.join([artist['name'] for artist in track['artists']]),
             'genre': genre
         })
+
     return similar_songs
 
-def classify_song(requests):
+#main function
+def classify_song(request):
+
     if request.method=='POST':
         form=SongForm(request.POST)
+
         if form.is_valid():
-            song=form.save(commit=False)
-            genre = classify_genre(song.title,song.artist)
+
+            song=form.save(commit=False) # false so that before adding the song, genre is identified
+            genre = classify_genre(song.title,song.artist) #to get the genre
             song.genre=genre
+            #genre field is set dynamically after form submission
+            #if commit=False is removed, an incomplete object is formed and would result in an error while saving to database
             song.save()
 
-            similar_songs = recommend_similar_songs(genre)
+            similar_songs = recommend_similar_songs(genre) 
+
             return render(request, 'songs/result.html', {'song': song, 'similar_songs': similar_songs})
     else:
         form = SongForm()
+
     return render(request, 'songs/classify.html', {'form': form})
+
+'''
+recommendations: parameters: genre,artist,tracks(any 1 must be provided); limit country(optional)
+response: tracks(key): name,id,artists,album,popularity,tracks uri (all values)
+
+search: parameters: query string(q)-artist/track/album, limit(optional)
+'''
